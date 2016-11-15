8f7e9715db4f2e386bd61a527b65c75dfd917225
Kurt Vincent Bautista <kurt.v@outlook.com>
2016-11-15 18:21:11 +0800

    Layout groups screen

---
+++ app/src/main/java/com/grpd/secb/mngr/GroupsActivity.java
@@ -0,0 +1,26 @@
+package com.grpd.secb.mngr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Spinner;
+
+public class GroupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem filter = menu.findItem(R.id.filter);
+        return  super.onCreateOptionsMenu(menu);
+    }
+}
