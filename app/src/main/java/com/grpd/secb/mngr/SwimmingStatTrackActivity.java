bb18c9f2d6d030d5eddc0236e561123f04ae3808
Kurt Vincent Bautista <kurt.v@outlook.com>
2016-11-24 21:10:55 +0800

    Create SwimmingStatTrack activity

---
+++ app/src/main/java/com/grpd/secb/mngr/SwimmingStatTrackActivity.java
@@ -0,0 +1,13 @@
+package com.grpd.secb.mngr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwimmingStatTrackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swimming_stat_track);
+    }
+}
